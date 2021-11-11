a2354b3557214cbe81919ca059c81ce9e52c31c3
SamRocan <SamClendenan@iCloud.com>
2021-11-11 14:43:06 +0000

    Urls and Home templates set up for each app

--- restaurantsite/main/urls.py
+++ restaurantsite/main/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('locations/', views.locations, name='locations'),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
--- restaurantsite/reservations/views.py
+++ restaurantsite/reservations/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def reservations_home(request):
+    return render(request, 'reservations/reservations_home.html')
