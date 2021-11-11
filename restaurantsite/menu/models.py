7b4ddf6d3efb26c73abd9522154a7dedb9f491c2
SamRocan <SamClendenan@iCloud.com>
2021-11-11 22:31:35 +0000

    Models updated and registered

--- restaurantsite/menu/models.py
+++ restaurantsite/menu/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    pass
+
+class MenuItem(models.Model):
+    pass
--- restaurantsite/reservations/models.py
+++ restaurantsite/reservations/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+import datetime
 
 # Create your models here.
+class Table(models.Model):
+    seats = models.PositiveIntegerField()
+    booked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.seats) + ": " + str(self.id)
+
+class Reservation(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, default="")
+    email = models.EmailField(default="")
+    # make sure that people < seats
+    # recommend for large bookings that they call/email to book
+    people = models.PositiveIntegerField(default=1)
+    date = models.DateTimeField(default=datetime.datetime.now())
+
+    def __str__(self):
+        return str(self.date) +": " + self.name + " (" + str(self.people) + ")"
+
+    class Meta:
+        ordering = ["date"]
