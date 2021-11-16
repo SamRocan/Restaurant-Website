1938631234761d46ed9e559b2fac44ee2a6c2806
SamRocan <SamClendenan@iCloud.com>
2021-11-16 09:27:32 +0000

    Interactive Reservation Map Ammended: - when time is selected reserved tables are not selectable.

--- restaurantsite/reservations/views.py
+++ restaurantsite/reservations/views.py
@@ -6,43 +6,29 @@ from datetime import datetime, timedelta, timezone
 def reservations_home(request):
     tables = Table.objects.all()
     reservations = Reservation.objects.all()
+
     #initialize all tables to not booked
     for table in tables:
         table.booked = False
-    start = (reservations.first().date)
-    end = (reservations.first().date + timedelta(minutes=100))
+        table.save()
+
     if(request.method=="POST"):
         #initialize all tables to not booked
         for table in tables:
             table.booked = False
+            table.save()
         startDate = request.POST.get("dateVar")
         startTime = request.POST.get("timeVar")
         people = int(request.POST.get("numVar"))
         dt = datetime.fromisoformat(startDate + " " + startTime+"+00:00")
 
-        for reservation in reservations:
-            tn = reservation.table.table_number
-            start = reservation.date - timedelta(minutes=100)
-            end = reservation.date + timedelta(minutes=100)
-            print(dt)
-            print(start)
-            print(end)
-            if(dt > end or dt < start):
-                table = tables.get(table_number=tn)
-                table.booked = False
-                table.save()
-            else:
-                table = tables.get(table_number=tn)
-                table.booked = True
-                table.save()
-
         tables = Table.objects.all()
 
         #If no of people is < half capacity of table it can't be booked
         for table in tables:
-            if (table.booked == True):
-                pass
-            elif(table.seats/2 > people):
+            #print(str(table.table_number) + " " + str(table.booked))
+
+            if(table.seats/2 > people):
                 table.booked = True
                 table.save()
             elif(table.seats < people):
@@ -52,6 +38,15 @@ def reservations_home(request):
                 table.booked = False
                 table.save()
 
+
+        for reservation in reservations:
+            tn = reservation.table.table_number
+            start = reservation.date - timedelta(minutes=100)
+            end = reservation.date + timedelta(minutes=100)
+            if(dt < end and dt > start):
+                for table in tables:
+                    if (tn == table.table_number):
+                        table.booked = True
     return render(request, 'reservations/reservations_home.html', {'tables':tables, 'reservations':reservations})
 
 def json_view(request):
