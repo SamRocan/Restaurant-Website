697c3edfa83862771266b5c62309bee06dc0f2f1
SamRocan <SamClendenan@iCloud.com>
2021-11-18 17:36:30 +0000

    Created Restaurant model for each restaurant location

--- restaurantsite/restaurants/models.py
+++ restaurantsite/restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django_countries.fields import CountryField
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    country = CountryField()
+    city = models.CharField(max_length=255)
+    street_address = models.CharField(max_length=255)
+    post_code = models.CharField(max_length=30)
