c055a9c459bc91f7219a0272fb99feb2999bc1d8
SamRocan <SamClendenan@iCloud.com>
2021-11-21 11:40:08 +0000

    Menu template updated and page added depending on menu

--- restaurantsite/menu/urls.py
+++ restaurantsite/menu/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.menu_home, name='menu_home')
+    path('', views.menu_home, name='menu_home'),
+    path('<str:slug>', views.menu, name='menu')
 ]
--- restaurantsite/menu/views.py
+++ restaurantsite/menu/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Menu, MenuSection, MenuItem
 # Create your views here.
 def menu_home(request):
-
     menu = Menu.objects.all()
     return render(request, 'menu/menu_home.html', {'menu':menu})
+
+def menu(request, slug):
+    menu_cat = get_object_or_404(Menu, slug=slug)
+    context = {'menu_cat':menu_cat}
+    return render(request,"menu/menu.html",context)
+
