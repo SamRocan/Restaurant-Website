9e2861ae69dc21a00119ff5ff6b951985f84ce47
SamRocan <SamClendenan@iCloud.com>
2021-11-21 14:55:23 +0000

    Allergens added to Menu Item model (multiple choice field)

--- restaurantsite/menu/models.py
+++ restaurantsite/menu/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from autoslug import AutoSlugField
+from multiselectfield import MultiSelectField
+
 
 # Create your models here.
 class Menu(models.Model):
@@ -18,14 +20,32 @@ class MenuSection(models.Model):
     def __str__(self):
         return str(self.name) + " ("+ str(self.menu.name) + ")"
 
+ALLERGENS = ((0, 'None'),
+             (1, 'Celery'),
+             (2, 'Crustaceans'),
+             (3, 'Fish'),
+             (4, 'Milk'),
+             (5, 'Mustard'),
+             (6, 'Peanuts'),
+             (7, 'Soya'),
+             (8, 'Gluten'),
+             (9, 'Eggs'),
+             (10, 'Lupin'),
+             (11, 'Molluscs'),
+             (12, 'Nuts'),
+             (13, 'Sesame Seeds'),
+             (14, 'Sulphur Dioxide')
+             )
 class MenuItem(models.Model):
     section = models.ForeignKey(MenuSection, on_delete=models.CASCADE, default="",  related_name='items')
     name = models.CharField(max_length=255, default="")
     description = models.CharField(max_length=255, default="")
+    allergens = MultiSelectField(choices=ALLERGENS, null=True, default=ALLERGENS[0][0])
     price = models.FloatField(
         validators=[MinValueValidator(0.00)],
         default=1
     )
+
 
     def __str__(self):
         return str(self.name) + " ("+ str(self.section.name) + ")"
