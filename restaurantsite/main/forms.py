e1e5e9386202409aeeee4ce0505d1ad70964c18c
SamRocan <SamClendenan@iCloud.com>
2021-11-22 11:38:28 +0000

    Ammended Contact form styling - Not finished, but 'attrs' have been added to forms.py to allow for forms to be styled with bootstrap classes

--- restaurantsite/main/forms.py
+++ restaurantsite/main/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    name = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    from_email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=True)
