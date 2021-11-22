8e60d0444dcc51c8e85941a9687040491fd1da34
SamRocan <SamClendenan@iCloud.com>
2021-11-22 10:29:06 +0000

    Added Footer - Added warning if reservation form is incorrect instead of redirecting to 404 page

--- restaurantsite/reservations/views.py
+++ restaurantsite/reservations/views.py
@@ -11,48 +11,52 @@ def reservations_home(request):
     tables = Table.objects.all()
     reservations = Reservation.objects.all()
     request.session['reservation'] = []
+    error = ""
     #initialize all tables to not booked
     for table in tables:
         table.booked = False
         table.save()
 
     if(request.method=="POST"):
-        #initialize all tables to not booked
-        for table in tables:
-            table.booked = False
-            table.save()
-        startDate = request.POST.get("dateVar")
-        startTime = request.POST.get("timeVar")
-        people = int(request.POST.get("numVar"))
-        request.session['reservation'] = [startDate, startTime, people]
-        dt = datetime.fromisoformat(startDate + " " + startTime+"+00:00")
-
-        tables = Table.objects.all()
-
-        #If no of people is < half capacity of table it can't be booked
-        for table in tables:
-            #print(str(table.table_number) + " " + str(table.booked))
-
-            if(table.seats/2 > people):
-                table.booked = True
-                table.save()
-            elif(table.seats < people):
-                table.booked = True
-                table.save()
-            else:
+        try:
+            #initialize all tables to not booked
+            for table in tables:
                 table.booked = False
                 table.save()
+            startDate = request.POST.get("dateVar")
+            startTime = request.POST.get("timeVar")
+            people = int(request.POST.get("numVar"))
+            request.session['reservation'] = [startDate, startTime, people]
+            dt = datetime.fromisoformat(startDate + " " + startTime+"+00:00")
+
+            tables = Table.objects.all()
+
+            #If no of people is < half capacity of table it can't be booked
+            for table in tables:
+                #print(str(table.table_number) + " " + str(table.booked))
+
+                if(table.seats/2 > people):
+                    table.booked = True
+                    table.save()
+                elif(table.seats < people):
+                    table.booked = True
+                    table.save()
+                else:
+                    table.booked = False
+                    table.save()
 
 
-        for reservation in reservations:
-            tn = reservation.table.table_number
-            start = reservation.date - timedelta(minutes=100)
-            end = reservation.date + timedelta(minutes=100)
-            if(dt < end and dt > start):
-                for table in tables:
-                    if (tn == table.table_number):
-                        table.booked = True
-    return render(request, 'reservations/reservations_home.html', {'tables':tables, 'reservations':reservations})
+            for reservation in reservations:
+                tn = reservation.table.table_number
+                start = reservation.date - timedelta(minutes=100)
+                end = reservation.date + timedelta(minutes=100)
+                if(dt < end and dt > start):
+                    for table in tables:
+                        if (tn == table.table_number):
+                            table.booked = True
+        except:
+            error = "Please fill out all fields"
+    return render(request, 'reservations/reservations_home.html', {'tables':tables, 'reservations':reservations, 'error':error})
 
 def reservation_confirm(request):
 
