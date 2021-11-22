1e4e231b80115597c4201363b7882a20b6760b45
SamRocan <SamClendenan@iCloud.com>
2021-11-22 18:56:12 +0000

    Updated styling on contact form

--- restaurantsite/main/forms.py
+++ restaurantsite/main/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
+    from_email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}),label="Email", required=True)
     name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=True)
