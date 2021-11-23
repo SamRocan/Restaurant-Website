853eff37739708ee7a1bdd337da5ed96a5b69029
SamRocan <SamClendenan@iCloud.com>
2021-11-23 21:09:16 +0000

    Added Intersection Observers to fade in content on scroll

--- restaurantsite/static/js/about_us_script.js
+++ restaurantsite/static/js/about_us_script.js
@@ -1,3 +1,42 @@
-var thehead = document.getElementById("test")
 
-thehead.innerText = "CHANGED"
+const SELECTOR = 'right'
+const SELECTOR2 = 'left'
+const SELECTOR3 = 'aniText'
+const ANIMATE_CLASS_NAME = 'animate';
+
+const animate = element =>  (
+    element.classList.add(ANIMATE_CLASS_NAME)
+)
+
+const isAnimated = element => (
+    element.classList.contains(ANIMATE_CLASS_NAME)
+)
+
+const intersectionObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+
+
+        if(entry.intersectionRatio > 0) {
+            animate(entry.target);
+        }
+
+    });
+});
+
+const elements = [].filter.call(
+    document.getElementsByClassName(SELECTOR),
+    element => !isAnimated(element, ANIMATE_CLASS_NAME)
+)
+const elements2 = [].filter.call(
+    document.getElementsByClassName(SELECTOR2),
+    element => !isAnimated(element, ANIMATE_CLASS_NAME)
+)
+const elements3 = [].filter.call(
+    document.getElementsByClassName(SELECTOR3),
+    element => !isAnimated(element, ANIMATE_CLASS_NAME)
+)
+
+elements.forEach((element) => console.log(element))
+elements.forEach((element) => intersectionObserver.observe(element));
+elements2.forEach((element) => intersectionObserver.observe(element));
+elements3.forEach((element) => intersectionObserver.observe(element));
